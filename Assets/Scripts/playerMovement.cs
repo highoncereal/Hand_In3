556743f91c8c520c9f93d71d80adfac8c68bb7c9
highoncereal <113972024+highoncereal@users.noreply.github.com>
2022-11-04 01:24:49 +0100

    Fixed Animation

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,17 +7,29 @@ public class playerMovement : MonoBehaviour
 {
     private Vector2 movement;
     private Rigidbody2D myBody;
+    private Animator myAnimator;
 
     [SerializeField] private int speed = 5;
 
     private void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     private void OnMovement(InputValue value)
     {
         movement = value.Get<Vector2>();
+
+        if (movement.x != 0 || movement.y != 0)
+        {
+            myAnimator.SetBool("IsWalking", true);
+        }
+
+        else
+        {
+            myAnimator.SetBool("IsWalking", false);
+        }
     }
 
     private void FixedUpdate()
